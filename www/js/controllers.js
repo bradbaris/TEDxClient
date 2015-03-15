7ee73489ebea08c132edcb6e96ef6d9ca5606b12
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-03-14 15:02:05 -1000

    fixed tedx-client path, removed second tedx-client subdirectory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+.controller('EventsCtrl', function($scope, Events) {
+  $scope.chats = Events.all();
+  $scope.remove = function(event) {
+    Events.remove(event);
   };
+})
+
+.controller('AgendaCtrl', function($scope, $stateParams, Events) {
+  $scope.event = Events.get($stateParams.eventId);
 });
