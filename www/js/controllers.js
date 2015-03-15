1223c8d272b4f0b36b92a2962aa3d8c72da77d36
Victor <viclee02@gmail.com>
2015-03-14 20:01:32 -1000

    fixed merge conflicts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('starter')
 
+
 .controller('EventsCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http, Events  ) {
   $scope.events = undefined;  //Events.list();
 
@@ -20,6 +21,37 @@ angular.module('starter')
 //  console.log($scope.events);
 }])
 
+// creating an application with angular with ionic module
+
+
+// define AgendaCtrl to make initialize ng-repeat
+.controller('AgendaCtrl', function($scope) {
+   $scope.agendas = []; 
+   for(var i=0; i<5; i++){ 
+    // test index =5, use length later
+    $scope.agendas[i] = {
+      name: i,
+      items: ['data']
+    }; 
+   } 
+   console.log("AgendaCtrl");
+
+
+// conditional if a given agenda is the selected agenda, deselect it, else, select the given agenda
+$scope.toggleAgenda = function(agenda){
+  if($scope.isAgendaShown(agenda)){
+    $scope.shownAgenda = null;
+  } else {
+    $scope.shownAgenda = agenda;
+  }
+};
+$scope.isAgendaShown = function(agenda){
+  return $scope.shownAgenda === agenda;
+  };
+
+})
+
 .controller('AgendaCtrl', function($scope, $stateParams, Events) {
   $scope.event = Events.get($stateParams.eventId);
 });
+
