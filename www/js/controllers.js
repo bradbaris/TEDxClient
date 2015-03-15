c6fba88ad2a30274e972a0a6b0ef754549a79773
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-03-14 18:48:48 -1000

    pull data from mongodb for events view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,25 @@
-angular.module('starter.controllers', [])
+angular.module('starter')
 
 
-.controller('EventsCtrl', function($scope, Events) {
-  $scope.chats = Events.all();
-  $scope.remove = function(event) {
-    Events.remove(event);
-  };
-})
+.controller('EventsCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http, Events  ) {
+  $scope.events = undefined;  //Events.list();
+
+  $http.get('https://floating-woodland-2582.herokuapp.com/api/events')
+    .then(function(result) {
+      console.log(result);
+      $scope.events = result.data;
+    });
+
+
+  $scope.name = "Sarah";
+  // $scope.remove = function(event) {
+  //   Events.remove(event);
+  // };
+
+  globaltest = $scope.events;
+
+//  console.log($scope.events);
+}])
 
 .controller('AgendaCtrl', function($scope, $stateParams, Events) {
   $scope.event = Events.get($stateParams.eventId);
