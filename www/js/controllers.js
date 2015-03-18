54293e7048aecca035b5e0ee8081d94b06fb95ad
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-03-17 19:20:46 -1000

    add event listing (rudimentary), have agenda list depend on selected event.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,14 +6,20 @@ angular.module('starter')
   // Events.list().success(callback);
   Events.get().success(function (data) {
     console.log(data);
-    $scope.event = data.filter(function (val) {
+    $scope.events = data;
+
+    $scope.shown_event = data.filter(function (val) {
       return val._id === "55075690e4b018f3e291f7c1";
     })[0];
-    console.log($scope.event);
+    console.log($scope.shown_event);
   });
 
   Agendas.list().success(function (data) {
-    $scope.agendas = data;
+    $scope.all_agendas = data;
+
+    $scope.agendas = data.filter(function(val) {
+      return (val.event_id === $scope.shown_event._id);
+    });
     console.log($scope.agendas);
   });
 
@@ -40,6 +46,34 @@ angular.module('starter')
     }
   };
   
+  $scope.toggleEvent = function(event) {
+    // if($scope.isEventShown(agenda)){
+    //   $scope.shownAgenda = null;
+    // } else {
+    //   $scope.shownAgenda = agenda;
+    // }
+    // console.log($scope.shown_event);
+
+    // if($scope.isAgendaShown(event)){
+    //   $scope.shownAgenda = null;
+    // } else {
+    //   $scope.shownAgenda = event;
+    // }
+
+
+    $scope.shown_event = event;
+    // console.log($scope.shown_event);
+
+    $scope.agendas = $scope.all_agendas.filter(function(val) {
+      return (val.event_id === $scope.shown_event._id);
+    });
+
+    console.log($scope.agendas);
+
+
+
+  }
+
   $scope.isAgendaShown = function(agenda){
     return $scope.shownAgenda === agenda;
   };
