ee302883f59160af4a2e18f12d897ea12467450e
Bradford Baris <bbaris@KUMORI.local>
2015-03-24 13:40:46 -1000

    wip/broken: testing splashscreens for ios/android

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'angularMoment'])
+angular.module('starter', ['ionic', 'angularMoment', 'ngFitText'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
